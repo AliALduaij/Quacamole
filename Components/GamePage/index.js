6db58057f64362de3d72ca716e96128bac2ad4fd
AliAlduaij <aliaalduaij@gmail.com>
2020-03-17 22:18:07 +0300

    little better

--- Components/GamePage/index.js
+++ Components/GamePage/index.js
@@ -7,7 +7,8 @@ import GameTimer from "../Timer/Timer";
 class GamePage extends Component {
   state = {
     showImage: Math.floor(Math.random() * 3) + 1,
-    score: 0
+    score: 0,
+    quacamole: 0
   };
 
   changeNumber = () => {
@@ -19,15 +20,38 @@ class GamePage extends Component {
   incrementScore = () => {
     this.setState({
       score: this.state.score + 5
-    }),
-      console.log("Score" + this.state.score);
+    });
   };
 
-  componentDidUpdate() {
-    setInterval(() => this.changeNumber(), 3000);
+  changeQuacamole = position => {
+    this.setState({
+      quacamole: position
+    });
+  };
+
+  returnQuacamole = () => {
+    this.setState({
+      quacamole: 0
+    });
+  };
+
+  gotIt = position => {
+    this.changeQuacamole(position);
+
+    this.incrementScore();
+    setTimeout(() => {
+      this.returnQuacamole();
+    }, 1000);
+
+    console.log("Quacamole" + this.state.quacamole);
+  };
+  // change did update to did mount , because wrote did update by mistake last time using tab .
+  componentDidMount() {
+    setInterval(() => this.changeNumber(), 1000);
   }
 
   render() {
+    //I didnt use mapping because every picture will have a condition !!
     return (
       <Container>
         <Text style={styles.time}>Time:</Text>
@@ -36,31 +60,52 @@ class GamePage extends Component {
         <View style={styles.square}>
           <View style={styles.circle}>
             {this.state.showImage == 1 ? (
-              <TouchableHighlight onPress={this.incrementScore}>
-                <Image
-                  source={require("../../assets/avocado.png")}
-                  style={styles.img}
-                />
+              <TouchableHighlight onPress={() => this.gotIt(1)}>
+                {this.state.quacamole == 1 ? (
+                  <Image
+                    source={require("../../assets/guacamole.png")}
+                    style={styles.img}
+                  />
+                ) : (
+                  <Image
+                    source={require("../../assets/avocado.png")}
+                    style={styles.img}
+                  />
+                )}
               </TouchableHighlight>
             ) : null}
           </View>
           <View style={styles.circle}>
             {this.state.showImage == 2 ? (
-              <TouchableHighlight onPress={this.incrementScore}>
-                <Image
-                  source={require("../../assets/avocado.png")}
-                  style={styles.img}
-                />
+              <TouchableHighlight onPress={() => this.gotIt(2)}>
+                {this.state.quacamole == 2 ? (
+                  <Image
+                    source={require("../../assets/guacamole.png")}
+                    style={styles.img}
+                  />
+                ) : (
+                  <Image
+                    source={require("../../assets/avocado.png")}
+                    style={styles.img}
+                  />
+                )}
               </TouchableHighlight>
             ) : null}
           </View>
           <View style={styles.circle}>
             {this.state.showImage == 3 ? (
-              <TouchableHighlight onPress={this.incrementScore}>
-                <Image
-                  source={require("../../assets/avocado.png")}
-                  style={styles.img}
-                />
+              <TouchableHighlight onPress={() => this.gotIt(3)}>
+                {this.state.quacamole == 3 ? (
+                  <Image
+                    source={require("../../assets/guacamole.png")}
+                    style={styles.img}
+                  />
+                ) : (
+                  <Image
+                    source={require("../../assets/avocado.png")}
+                    style={styles.img}
+                  />
+                )}
               </TouchableHighlight>
             ) : null}
           </View>
---
+++ Components/Timer/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  timerPosition: {
+    top: 273,
+    right: 100
+  }
+});
+
+export default styles;
